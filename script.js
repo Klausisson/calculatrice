aee04efe7216b8ce16d7994997aad79d5d77228b
Simon.B <simon.biela@hotmail.com>
2017-07-21 10:36:24 +0200

    Creation de la calculatrice, finission du code html et css, parti codage js en cours

---
+++ script.js
@@ -0,0 +1,18 @@
+// 0.1 recupéré les onclick et leurs attribu
+// 0.2 les stocker dans une variable
+// 0.3 affiché le contenu de la variable dans l'écran
+// 1.1 créer une fonction qui vas calculer la variable
+// 1.2 affiché le résultat de la fonction dans l'écran
+var input = [];
+
+function calculFinal(){
+
+}
+
+function afficheur(){
+
+}
+
+function saisi(){
+
+}
