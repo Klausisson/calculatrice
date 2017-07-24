c40ce137f3c1ed3923d7809f60f8c6067b5eb232
Simon.B <simon.biela@hotmail.com>
2017-07-24 14:15:26 +0200

    Calculatrice terminé

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-// 0.1 recupéré les onclick et leurs attribu
-// 0.2 les stocker dans une variable
-// 0.3 affiché le contenu de la variable dans l'écran
-// 1.1 créer une fonction qui vas calculer la variable
-// 1.2 affiché le résultat de la fonction dans l'écran
-var input = [];
+var operation = "";
+var result = 0;
 
-function calculFinal(){
-
+function  add(element) {
+  console.log(element.value);
+  //stocker
+  operation += element.value;
+  document.getElementById("afficheur").value = operation;
+  //afficher
+}
+function stop(){
+ // operation // mettre la string (operation) VIDE
+ location.reload(); // on rafraichi la page afin de vidé la variable operation.
 }
 
-function afficheur(){
+function egal(oper) {
+  //stocker resultat
+  result = eval(operation);
+  document.getElementById("afficheur").value = result;
 
+  //affiche resultat
 }
 
-function saisi(){
+//calculette
 
-}
+//1  click sur un nombre
+//1.1 stock la valeur du nombre
+//1.2 affiche le nombre dans afficheur
+
+
+//2 click sur operateur
+//2.1 stock la valeur du ope dans var nombre
+//2.2 affiche le nombre dans afficheur
+
+//3 apui sur egal
+//3.1 evalue operation
+//3.2 afficher sur afficheur
